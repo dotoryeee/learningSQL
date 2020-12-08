691818327520e42bfdcc6e40f97e520ae588c6ba
dotoryeee <bayer@kakao.com>
2020-12-08 10:25:40 +0900

    문제풀이

--- 20-12-07.sql
+++ 20-12-07.sql
@@ -70,6 +70,8 @@ insert into MY_EMPLOYEE values (4, 'Noewman', 'Chad', 'cnewman', 750);
 insert into MY_EMPLOYEE values (5, 'Ropeburn', 'Audrey', 'aropebur', 1550);
 --5
 select * from MY_EMPLOYEE;
+--6
+update MY_EMPLOYEE set USERID = substr(FIRST_NAME, 1, 1),
 --10
 update MY_EMPLOYEE set LAST_NAME = 'Drexler' where id = 3;
 --11
@@ -136,3 +138,79 @@ alter table EMP
 drop unused columns;
 
 select * from EMP
+-------------------------------
+12-08에 진행한 문제 풀이
+DML-데이터 조작어(insert, update, delete)
+테이블 안에 저장된 행을 조작, 행을 추가, 수정, 삭제
+insert into 테이블명[(컬럼명1, 컬럼명2...)]
+values(val1, val2,...);
+update 테이블명 set 컬럼명1=값1, 컬럼명2=값2
+where 조건;
+delete [from] 테이블명 where 조건;
+트랜잭션: 데이터의 일관성을 제공하기 위해서 사용
+작업단위
+commit - 쓰기 완료. dml 명령 결과가 모든 사용자에게 적용
+rollback - 쓰기 취소. dml 명령 취소
+savepoint 이름- 중단점. rollback시 취소 위치를 지정
+primary key: unique(중복안됨)  + not null(널 허용안함)
+    그 줄의 대표값
+8-1>
+create table my_employee(
+id number(4) primary key,
+last_name varchar2(25),
+first_name varchar2(25),
+userid varchar2(8),
+salary number(9, 2)
+);
+create sequence seq_my_employee;
+select seq_my_employee.nextval from dual;
+8-2>
+desc my_employee;
+8-3>
+insert into my_employee values(seq_my_employee.nextval,
+'Patel', 'Ralph',null,895);
+insert into my_employee values(seq_my_employee.nextval,
+'Dancs', 'Betty',null,860);
+insert into my_employee values(seq_my_employee.nextval,
+'Biri', 'Ben',null,1100);
+8-6>
+update my_employee set userid=lower(concat(substr(first_name,1,1),substr(last_name,1,7)));
+9장
+DDL - 데이터 정의어. 테이블(시퀀스, 인덱스, 뷰, 동의어..) 생성, 수정, 삭제
+테이블 생성
+create table 테이블명(
+컬럼명 타입 제약조건,
+...
+);
+타입: 문자열(char, varchar2), 숫자(number), 날짜(date)...
+테이블 수정
+컬럼 추가
+alter table 테이블명
+add (컬럼명 타입 제약조건)
+컬럼 수정-컬럼의 크기나 타입을 수정
+alter table 테이블명
+modify (컬럼명 타입 제약조건)
+컬럼 삭제
+alter table 테이블명
+drop column 컬럼명
+-------------
+사용하지 않는 컬럼들을 set unused 설정한 뒤
+alter table 테이블명
+drop unused columns;
+테이블 삭제
+drop table 테이블명
+9-6>
+create table employees2 as
+select employee_id id, first_name, last_name, salary, department_id dept_id
+from employees;
+9-10>
+alter table employees2
+drop column first_name;
+9-12>
+alter table employees2
+set UNUSED (salary);
+alter table employees2
+set UNUSED (dept_id);
+alter table employees2
+drop unused columns;
+
